44a77e2d3213f2fb2755878337ec993227b904db
pangcunqi1988 <pangcunqi@163.com>
2020-05-03 14:34:49 +0800

    Update PackageMgr.h

--- PackageMgr.h
+++ PackageMgr.h
@@ -14,7 +14,7 @@ class PackageMgr
 {
 public:
 	static	PackageMgr& GetInstance();
-	bool		InitPackage(const char* pszDirectory);
+	bool	InitPackage(const char* pszDirectory);
 	FILE*	Myfopen(const char* pszFileName, const char* pszOperator);
 	char*	Myfgets(char* pszBuffer, int nSize, FILE * fp);
 
@@ -26,14 +26,13 @@ private:
 	struct FileInfo
 	{
 		string	strFileName;
-		int		nOffset;
-		int		nSize;
-		int		nReadPos;
+		int	nOffset;
+		int	nSize;
+		int	nReadPos;
 	};
 	vector<FileInfo> m_vecFileInfo;
 	char*					m_pBuffer;
-	int						m_pWritePos;
-
+	int					m_pWritePos;
 	string					m_strDirectory;
 };
 
